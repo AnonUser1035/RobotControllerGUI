9c2dac823bfdf46284a320779a0ba69910398f04
dgzct11 <60042905+dgzct11@users.noreply.github.com>
2021-09-27 09:22:16 -0500

    Update Main.java

--- src/main/java/app/Main.java
+++ src/main/java/app/Main.java
@@ -2,11 +2,10 @@ package app;
 import edu.wpi.first.networktables.NetworkTable;
 import edu.wpi.first.networktables.NetworkTableEntry;
 import edu.wpi.first.networktables.NetworkTableInstance;
-
 public class Main{
     public static void main(String[] args) {
         System.out.println("hello world");
-        
+    
     }
     public static void gg(){}
 }
