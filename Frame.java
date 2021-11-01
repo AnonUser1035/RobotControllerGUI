75cafb6ba2e1006722b66fa140b0dc332928d9ad
AnonUser1035 <ryan.bohluli@gmail.com>
2021-11-01 09:43:08 -0500

    added a basic frame

---
+++ Frame.java
@@ -0,0 +1,13 @@
+import javax.swing.*;
+
+public class Frame {
+    JFrame frame;
+    public Frame() {
+        frame = new JFrame("test");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(800,600);
+        frame.setVisible(true);
+    }
+
+    
+}
