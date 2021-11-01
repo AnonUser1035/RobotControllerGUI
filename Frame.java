bbfcea798a161ce031c504f04e453f05f1cc6461
AnonUser1035 <ryan.bohluli@gmail.com>
2021-11-01 10:28:43 -0500

    image background

--- Frame.java
+++ Frame.java
@@ -1,3 +1,7 @@
+import java.io.File;
+import java.awt.Graphics;
+
+import javax.imageio.ImageIO;
 import javax.swing.*;
 
 public class Frame {
@@ -5,9 +9,16 @@ public class Frame {
     public Frame() {
         frame = new JFrame("test");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setSize(800,600);
+        frame.setSize(2770, 1440);
+        try {
+            ImageIcon img = new ImageIcon(ImageIO.read(new File("FRC 2020 Field Top.png")));
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+    } 
+
+    public void updateFrame() {
         frame.setVisible(true);
     }
-
-    
 }
